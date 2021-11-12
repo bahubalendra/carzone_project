0554abf7b427e3de8b2b158dacf02def4810bae7
Bahubalendra Barik <60384737+bahubalendra@users.noreply.github.com>
2021-11-12 11:26:40 +0530

    first commit

---
+++ pages/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Team(models.Model):
+    first_name = models.CharField(max_length=300)
+    last_name = models.CharField(max_length=300)
+    designation = models.CharField(max_length=300)
+    photo = models.ImageField(upload_to = 'photos/%Y/%m/%d/')
+    facebook_link = models.URLField(max_length=300)
+    twitter_link = models.URLField(max_length=300)
+    google_plus_link = models.URLField(max_length=300)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.first_name
+    
+    
